d27e41b542e6c5c52fb85deeb8b5f3467eff0a8d
Pieter Provoost <pieterprovoost@gmail.com>
2015-11-18 18:18:36 +0100

    visualize orca data

--- demo/orca.R
+++ demo/orca.R
@@ -14,3 +14,17 @@ valid_id <- valid_taxon$valid_id[1]
 valid_path <- paste0("'", valid_taxon$storedpath[1], valid_taxon$valid_id[1], "x%'")
 
 data <- dbGetQuery(con, paste0("select p.* from portal.points_ex p left join obis.tnames t on p.valid_id = t.id where t.id = ", valid_id, " or t.storedpath like ", valid_path))
+
+# visualize
+
+require(mapdata)
+require(ggplot2)
+
+world <- map_data(map="world")
+
+g <- ggplot(world, aes(long, lat)) +
+  geom_polygon(aes(group=group), fill="gray90", color="gray90") +
+  labs(x="", y="") + theme(panel.background=element_blank()) + coord_fixed(ratio=1)
+
+g + geom_point(data=data, aes(x=longitude, y=latitude, colour=monthcollected)) +
+  scale_colour_gradientn(colours=c("#7ED5C8", "#F2D391", "#EF6E4A"))
