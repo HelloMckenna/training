182be3e259be46a92eabcbae83404580b8d8864e
Pieter Provoost <pieterprovoost@gmail.com>
2016-06-07 09:41:29 +0200

    cleanup

--- calculations/diversity/script.R
+++
@@ -1,5 +0,0 @@
-require(dplyr)
-
-data <- read.table("occurrence.txt", sep="\t", header=TRUE, stringsAsFactors=FALSE)
-
-stations <- data %>% group_by(eventID) %>% summarize(diversity = length(unique(scientificName)))
--- calculations/robert_2/script.R
+++
@@ -1,8 +0,0 @@
-require(openxlsx)
-require(dplyr)
-
-data <- read.xlsx("Arthropoda_matchRecordsToTaxonomy.xlsx", 1)
-taxonomy <- read.xlsx("Arthropoda_matchRecordsToTaxonomy.xlsx", 2)
-
-data <- left_join(data, taxonomy, by="AphiaID")
-write.xlsx(data, file="output.xlsx")
